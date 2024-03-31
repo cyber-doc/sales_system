f56e1bfef5af6f87a89a3a99562243850556e60b
Ali <aligr.worku@gmail.com>
2024-03-31 14:59:29 +0330

    deleted an unnecessary file (test.py)

--- test.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector as mysql
-from config import config
-print('this is a test!!')
-with (mysql.connect(**config)) as connection:
-    with (connection.cursor(buffered=True)) as cursor:
-        result = cursor.execute(
-            "INSERT INTO sales_system.users (first_name, last_name, email, password, creat_date, last_update) VALUES (Nazanin, Tehrani, nazetehroon\@gmail.com, 2022-05-14, 2022-06-08)")
-        connection.commit()
-        print(result)
